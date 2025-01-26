b78487d72bb0a9286c6ea380344818c5a758d368
dzamoranouniandes <d.zmorano@uniandes.edu.co>
2025-01-25 23:19:14 -0500

    Se elimina comentario To do de función load_data().

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
     raw_data_df =  pd.read_csv('datos_energia.csv', sep=',', encoding='utf-8')
     
     raw_data_df['time'] = pd.to_datetime(raw_data_df['time'])
